d4a03750e6f338fffe657c44174004ef1ce15501
wooooozin <wooooozin@gmail.com>
2022-12-01 05:16:33 +0900

    코어데이터 비동기처리를 위한 Model 생성

---
+++ LetsTodo/LetsTodo/Models/TodoModel.swift
@@ -0,0 +1,16 @@
+//
+//  TodoModel.swift
+//  LetsTodo
+//
+//  Created by 효우 on 2022/12/01.
+//
+
+import Foundation
+
+struct TodoModel {
+    var title: String?
+    var memo: String?
+    var id: UUID?
+    var date: Date?
+    var time: String?
+}
